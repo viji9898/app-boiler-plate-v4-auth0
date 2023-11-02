f9eb0484d6fb5054ad7bfec9943d9886a23eb677
viji9898 <vijitha.wijesuriya@gmail.com>
2023-11-02 15:26:06 +0300

    remove-netlify-identity

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,12 +4,8 @@ import { Hero } from "../components/landingPage/hero";
 import { DynamicMetaTags } from "../utils/dynamicMetaTags";
 import { FaunaBd } from "../components/landingPage/faunaDb";
 import React from "react";
-import { AuthContext } from "../utils/AuthContext";
-import { AuthIdentity } from "../components/navigation/authIdentity";
 
 export const LandingPage = () => {
-  const { user } = React.useContext(AuthContext);
-  console.log(user);
   return (
     <div
       style={{
@@ -21,7 +17,6 @@ export const LandingPage = () => {
       <DynamicMetaTags ogTitle={`boiler-plate-v3`} ogImage={""} />
       <Row style={{ maxWidth: "900px", height: "70vh" }}>
         <Hero />
-        <AuthIdentity />
         <Col style={{ textAlign: "center" }} span={24}>
           <h2>{"Test Envirnoment"}</h2>
           <p>Event: testing commits from mac mini 2014</p>
